b2bb0ed65af2fe3b6dc19e0e2db08628eccd1d46
aislinggallagher <gallagheraisling01@gmail.com>
2023-11-20 18:41:20 +0000

    dbpedia test

--- src/server/sparql/sampo/sparql_queries/SparqlQueriesPerspective1.js
+++ src/server/sparql/sampo/sparql_queries/SparqlQueriesPerspective1.js
@@ -2,61 +2,31 @@ const perspectiveID = 'perspective1'
 
 export const workProperties = `
     {
-      ?id skos:prefLabel ?prefLabel__id .
+      ?id rdfs:label ?prefLabel__id .
       BIND(?prefLabel__id AS ?prefLabel__prefLabel)
-      BIND(CONCAT("/${perspectiveID}/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
+      BIND(CONCAT("/${perspectiveID}/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1"))
+    AS ?prefLabel__dataProviderUrl)
       BIND(?id as ?uri__id)
       BIND(?id as ?uri__dataProviderUrl)
       BIND(?id as ?uri__prefLabel)
     }
     UNION
     {
-      ?id ^mmm-schema:manuscript_work ?manuscript__id .
-      ?manuscript__id skos:prefLabel ?manuscript__prefLabel .
-      BIND(CONCAT("/manuscripts/page/", REPLACE(STR(?manuscript__id), "^.*\\\\/(.+)", "$1")) AS ?manuscript__dataProviderUrl)
+      ?id dbo:genre ?genre__id .
+      ?genre__id rdfs:label ?genre__prefLabel .
+      FILTER(LANG(?genre__prefLabel) = 'en')
     }
     UNION
     {
-      ?id  ^mmm-schema:manuscript_work/crm:P46i_forms_part_of ?collection__id .
-      ?collection__id skos:prefLabel ?collection__prefLabel .
-      BIND(CONCAT("/collections/page/", ENCODE_FOR_URI(REPLACE(STR(?collection__id), "^.*\\\\/(.+)", "$1"))) AS ?collection__dataProviderUrl)
+      ?id dbo:occupation ?occupation__id .
+      ?occupation__id rdfs:label ?occupation__prefLabel .
+      FILTER(LANG(?occupation__prefLabel) = 'en')
     }
     UNION
     {
-      ?id ^mmm-schema:manuscript_work/crm:P45_consists_of ?material__id .
-      ?material__id skos:prefLabel ?material__prefLabel .
-    }
-    UNION
-    {
-      ?id dct:source ?source__id .
-      ?source__id skos:prefLabel ?source__prefLabel .
-    }
-    UNION
-    {
-      ?id ^frbroo:R16_initiated/(mmm-schema:carried_out_by_as_possible_author|mmm-schema:carried_out_by_as_author) ?author__id .
-      ?author__id skos:prefLabel ?author__prefLabel .
-      BIND(CONCAT("/actors/page/", REPLACE(STR(?author__id), "^.*\\\\/(.+)", "$1")) AS ?author__dataProviderUrl)
-    }
-    UNION
-    {
-      ?id ^frbroo:R19_created_a_realisation_of/frbroo:R17_created ?expression__id .
-      ?expression__id skos:prefLabel ?expression__prefLabel .
-      OPTIONAL {
-        ?expression__id crm:P72_has_language ?language__id .
-        ?expression__id dct:source ?language__source__id .
-        ?language__source__id skos:prefLabel ?language__source__prefLabel .
-        ?language__id skos:prefLabel ?language__prefLabel .
-      }
-      BIND(CONCAT("/expressions/page/", REPLACE(STR(?expression__id), "^.*\\\\/(.+)", "$1")) AS ?expression__dataProviderUrl)
-    }
-    UNION
-    {
-      ?id ^mmm-schema:manuscript_work/^crm:P108_has_produced/crm:P4_has_time-span ?productionTimespan__id .
-      ?productionTimespan__id skos:prefLabel ?productionTimespan__prefLabel .
-      ?productionTimespan__id dct:source ?productionTimespan__source__id .
-      ?productionTimespan__source__id skos:prefLabel ?productionTimespan__source__prefLabel .
-      OPTIONAL { ?productionTimespan__id crm:P82a_begin_of_the_begin ?productionTimespan__start }
-      OPTIONAL { ?productionTimespan__id crm:P82b_end_of_the_end ?productionTimespan__end }
+      ?id dbo:almaMater ?almaMater__id .
+      ?almaMater__id rdfs:label ?almaMater__prefLabel .
+      FILTER(LANG(?almaMater__prefLabel) = 'en')
     }
 `
 
