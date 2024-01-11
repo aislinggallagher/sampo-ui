0c574c24ea5c488ce7a3559f47aeef9e90069337
aislinggallagher <gallagheraisling01@gmail.com>
2024-01-11 22:56:01 +0000

    perspective 1 text query

--- src/server/sparql/sampo/sparql_queries/SparqlQueriesPerspective1.js
+++ src/server/sparql/sampo/sparql_queries/SparqlQueriesPerspective1.js
@@ -1,33 +1,15 @@
 const perspectiveID = 'perspective1'
 
 export const workProperties = `
-    {
-      ?id rdfs:label ?prefLabel__id .
-      BIND(?prefLabel__id AS ?prefLabel__prefLabel)
-      BIND(CONCAT("/${perspectiveID}/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1"))
-    AS ?prefLabel__dataProviderUrl)
-      BIND(?id as ?uri__id)
-      BIND(?id as ?uri__dataProviderUrl)
-      BIND(?id as ?uri__prefLabel)
-    }
-    UNION
-    {
-      ?id dbo:genre ?genre__id .
-      ?genre__id rdfs:label ?genre__prefLabel .
-      FILTER(LANG(?genre__prefLabel) = 'en')
-    }
-    UNION
-    {
-      ?id dbo:occupation ?occupation__id .
-      ?occupation__id rdfs:label ?occupation__prefLabel .
-      FILTER(LANG(?occupation__prefLabel) = 'en')
-    }
-    UNION
-    {
-      ?id dbo:almaMater ?almaMater__id .
-      ?almaMater__id rdfs:label ?almaMater__prefLabel .
-      FILTER(LANG(?almaMater__prefLabel) = 'en')
-    }
+{
+?id rdfs:label ?prefLabel__id .
+BIND(?prefLabel__id AS ?prefLabel__prefLabel)
+BIND(CONCAT("/${perspectiveID}/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1"))
+AS ?prefLabel__dataProviderUrl)
+BIND(?id as ?uri__id)
+BIND(?id as ?uri__dataProviderUrl)
+BIND(?id as ?uri__prefLabel)
+}
 `
 
 export const knowledgeGraphMetadataQuery = `
--- src/server/sparql/sampo/sparql_queries/SparqlQueriesPrefixes.js
+++ src/server/sparql/sampo/sparql_queries/SparqlQueriesPrefixes.js
@@ -1,14 +1,12 @@
 export const prefixes = `
   PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
   PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
   PREFIX crm: <http://erlangen-crm.org/current/>
   PREFIX crm-org: <http://www.cidoc-crm.org/cidoc-crm/>
   PREFIX owl: <http://www.w3.org/2002/07/owl#>
   PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
   PREFIX text: <http://jena.apache.org/text#>
   PREFIX dct: <http://purl.org/dc/terms/>
-  PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
   PREFIX sch: <http://schema.org/>
   PREFIX geosparql: <http://www.opengis.net/ont/geosparql#>
   PREFIX frbroo: <http://erlangen-crm.org/efrbroo/>
@@ -23,4 +21,11 @@ export const prefixes = `
   PREFIX semparls: <http://ldf.fi/schema/semparl/>
   PREFIX dbr: <http://dbpedia.org/resource/>
   PREFIX dbo: <http://dbpedia.org/ontology/>
+  PREFIX cidoc: <http://erlangen-crm.org/current/>
+  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+  PREFIX geo:   <http://www.opengis.net/ont/geosparql#>
+  PREFIX b2022: <https://ont.virtualtreasury.ie/ontology#>
+  PREFIX rr: <http://www.w3.org/ns/r2rml#>
+  PREFIX geom: <http://data.ordnancesurvey.co.uk/ontology/geometry/>
+
 `
