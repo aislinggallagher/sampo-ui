d7b9f69296c9c5f5fa33b2b776437582afe63ec3
aislinggallagher <gallagheraisling01@gmail.com>
2024-03-15 05:36:57 +0000

    clusters working

--- src/client/components/facet_results/mapbox/BaroniesMap.js
+++ src/client/components/facet_results/mapbox/BaroniesMap.js
@@ -1,8 +1,23 @@
-import React, { useState, useEffect } from 'react'
-import ReactMapGL, { Marker, Popup } from 'react-map-gl'
+import React, { useState, useEffect, useRef } from 'react'
+import ReactMapGL, { Marker, Popup, FlyToInterpolator } from 'react-map-gl'
+import Supercluster from 'supercluster'
+import useSupercluster from 'use-supercluster'
+import 'mapbox-gl/dist/mapbox-gl.css'
 import data from './geoJson/centre-points/baronies_centers.json'
 
 const BaroniesMap = () => {
+  const points = data.map(item => ({
+    type: "Feature",
+    properties: { cluster: false, name_en: item.name_en, name_ga: item.name_ga, co_name: item.co_name },
+    geometry: {
+      type: "Point",
+      coordinates: [
+        parseFloat(item.coordinates[0]),
+        parseFloat(item.coordinates[1])
+      ]
+    }
+  }))
+
   const [viewport, setViewport] = useState({
     width: '100%',
     height: '100%',
@@ -10,45 +25,100 @@ const BaroniesMap = () => {
     longitude: -7,
     zoom: 6
   })
+  const mapRef = useRef()
+  const bounds = mapRef.current
+    ? mapRef.current
+        .getMap()
+        .getBounds()
+        .toArray()
+        .flat()
+    : null
+
+  // get clusters
+  const { clusters, supercluster } = useSupercluster({
+    points,
+    bounds,
+    zoom: viewport.zoom,
+    options: { radius: 75, maxZoom: 20 }
+  })
 
   const [selectedMarker, setSelectedMarker] = useState(null)
-
   return (
     <ReactMapGL
       {...viewport}
-      mapStyle='mapbox://styles/aislinggal/clthhdhah002h01ph614cdamz'
       mapboxApiAccessToken='pk.eyJ1IjoiYWlzbGluZ2dhbCIsImEiOiJjbG82ZzIzNXgwb3J2MmtvMmVrMDh3b3E2In0.wzGjrfYTRwGdbuuWSDCBuw'
-      onViewportChange={(viewport) => setViewport(viewport)}
+      onViewportChange={setViewport}
+      mapStyle='mapbox://styles/aislinggal/clthhdhah002h01ph614cdamz'
+      ref={mapRef}
     >
-      {data.map((marker, index) => (
-        <Marker key={index} longitude={marker.coordinates[0]} latitude={marker.coordinates[1]}>
-          <div style={{ position: 'absolute', left: -7.5, top: -7.5, width: '15px', height: '15px', backgroundColor: 'blue', borderRadius: '50%', cursor: 'pointer' }} onClick={() => setSelectedMarker(marker)} />
-        </Marker>
-      ))}
+      {clusters.map(cluster => {
+      // every cluster point has coordinates
+        const [longitude, latitude] = cluster.geometry.coordinates
+        // the point may be either a cluster or a point
+        const {
+          cluster: isCluster,
+          point_count: pointCount
+        } = cluster.properties
+        // we have a cluster to render
+        if (isCluster) {
+          return (
+            <Marker
+              key={`cluster-${cluster.id}`}
+              latitude={latitude}
+              longitude={longitude}
+              offsetLeft={-20} // Adjust as needed
+              offsetTop={-20} // Adjust as needed
+            >
+              <div
+                className="cluster-marker"
+                style={{
+                  width: `${10 + (pointCount / points.length) * 20}px`,
+                  height: `${10 + (pointCount / points.length) * 20}px`,
+                  color: '#fff',
+                  background: '#1978c8',
+                  borderRadius: '50%',
+                  padding: '10px',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center'
+                }}
+                onClick={() => {
+                  const expansionZoom = Math.min(
+                    supercluster.getClusterExpansionZoom(cluster.id),
+                    20
+                  )
+                  setViewport({
+                    ...viewport,
+                    latitude,
+                    longitude,
+                    zoom: expansionZoom,
+                    transitionInterpolator: new FlyToInterpolator({
+                      speed: 2
+                    }),
+                    transitionDuration: 'auto'
+                  })
+                }}
+              >
+                {pointCount}
+              </div>
+            </Marker>
+          )
+        }
 
-      {selectedMarker && (
-        <Popup
-          longitude={selectedMarker.coordinates[0]}
-          latitude={selectedMarker.coordinates[1]}
-          onClose={() => setSelectedMarker(null)}
-          closeOnClick={false} // Prevent popup from closing when clicked outside
-        >
-          <div className='popup'>
-            <h2>{selectedMarker.name_en}</h2>
-            <h2>{selectedMarker.name_ga}</h2>
-            <div className='popup-content'>
-              <p>Latitude: {selectedMarker.coordinates[1]}</p>
-              <p>Longitude: {selectedMarker.coordinates[0]}</p>
-            </div>
-            <div className='popup-actions'>
-              <button className='popup-button'>Action 1</button>
-              <button className='popup-button'>Action 2</button>
-            </div>
-          </div>
-        </Popup>
-      )}
+        // we have a single point to render
+        return (
+          <Marker
+            key={`crime-${cluster.properties.name_en}`}
+            latitude={latitude}
+            longitude={longitude}
+            offsetLeft={-20} // Adjust as needed
+            offsetTop={-20} // Adjust as needed
+          >
+            <div style={{ position: 'absolute', left: -7.5, top: -7.5, width: '15px', height: '15px', backgroundColor: 'blue', borderRadius: '50%', cursor: 'pointer' }} />
+          </Marker>
+        )
+      })}
     </ReactMapGL>
   )
 }
-
 export default BaroniesMap
